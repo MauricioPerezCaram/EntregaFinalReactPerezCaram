8760b9fead98be144fdb16f0534b4db4a756de14
MauricioPerezCaram <perezcarammauricio@gmail.com>
2023-11-06 19:36:38 -0300

    carrito agregado

--- src/components/Carrito.jsx
+++ src/components/Carrito.jsx
@@ -11,31 +11,26 @@ function Carrito() {
   return (
     <div className="container">
       <h1>Carrito</h1>
+      {carrito.map((prod) => (
+        <div key={prod.id}>
+          <h3>{prod.nombre}</h3>
+          <p>$ {prod.precio}</p>
+          <p>Cantidad: {prod.cantidad}</p>
+          <p>Total: $ {prod.precio * prod.cantidad}</p>
+        </div>
+      ))}
       {carrito.length > 0 ? (
-        carrito.map((prod) => (
-          <div key={prod.id}>
-            <h3>{prod.nombre}</h3>
-            <p>$ {prod.precio}</p>
-            <p>Cantidad: {prod.cantidad}</p>
-            <p>Total: $ {prod.precio * prod.cantidad}</p>
-          </div>
-        ))
-      ) : (
-        <h2>Agrega algo al carrito</h2>
-      )}
-      {carrito.length > 0 && (
         <>
           <h2>Total agregado al carrito: $ {precioTotal()}</h2>
-          <button onClick={handleVaciar}>Vaciar carrito</button>
+          <button onClick={handleVaciar} className="agregar-al-carrito">
+            Vaciar carrito
+          </button>
         </>
+      ) : (
+        <h2>Carrito esta vacio</h2>
       )}
     </div>
   );
 }
 
 export default Carrito;
-
-{
-  /* <h2>Total agregado al carrito: $ {precioTotal()}</h2>
-<button onClick={handleVaciar}>Vaciar Carrito</button> */
-}
